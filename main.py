6c812c949978ba460e7f0d23b72e49768b4312b3
chumardo <dachigiorgadze98@gmail.com>
2022-08-31 20:25:32 +0400

    tools with icons and hover

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from cgitb import text
 from tkinter import *
 import tkinter
 from PIL import ImageTk, Image
+from idlelib.tooltip import Hovertip
 
 window = Tk()
+window.title("Book Information Store")
 window.iconphoto(False, PhotoImage(file='images/icon.png'))
 window.minsize(1300,700)
 window.configure(bg='lightblue')
@@ -12,7 +14,7 @@ window.configure(bg='lightblue')
 icon_image = Image.open("images/icon.png")
 book_photo = ImageTk.PhotoImage(icon_image)
 book_label = Label(image=book_photo, bg='lightblue')
-book_label.place(x=600, y=10)
+book_label.place(x=600, y=15)
 
 info_frame = LabelFrame(window, text="Book Information", relief=RIDGE, bg='#a9c0e2', font=('arial', 15, 'bold'), fg='black')
 info_frame.place(x=10, y=10, width=460, height=110)
@@ -46,5 +48,39 @@ publisher_entry.grid(row=1, column=4, padx=5, pady=5)
 
 list = Listbox(window, height=10, width=35)
 list.place(x=10, y=130, height=560, width=1280)
+
+
+tools_frame = LabelFrame(window, text="Tools", relief=RIDGE, bg='#a9c0e2', font=('arial', 15, 'bold'), fg='black')
+tools_frame.place(x=830, y=10, width=460, height=110)
+
+view_image = PhotoImage(file='images/view_all.png', height=50, width=50)
+view_btn = Button(tools_frame, image=view_image, bg='#a9c0e2', fg='black')
+view_btn_hover = Hovertip(view_btn, 'View All', hover_delay=10)
+view_btn.grid(row=0, column=0, padx=15, pady=5)
+
+search_image = PhotoImage(file='images/search.png', height=50, width=50)
+search_btn = Button(tools_frame, image=search_image, bg='#a9c0e2', fg='black')
+search_btn_hover = Hovertip(search_btn, 'Search Entry', hover_delay=10)
+search_btn.grid(row=0, column=1, pady=5)
+
+add_image = PhotoImage(file='images/add.png', height=50, width=50)
+add_btn = Button(tools_frame, image=add_image, bg='#a9c0e2', fg='black')
+add_btn_hover = Hovertip(add_btn, 'Add Entry', hover_delay=10)
+add_btn.grid(row=0, column=2, padx=10, pady=5)
+
+update_image = PhotoImage(file='images/update.png', height=50, width=50)
+update_btn = Button(tools_frame, image=update_image, bg='#a9c0e2', fg='black')
+update_btn_hover = Hovertip(update_btn, 'Update', hover_delay=10)
+update_btn.grid(row=0, column=3, padx=10, pady=5)
+
+delete_image = PhotoImage(file='images/delete.png', height=50, width=50)
+delete_btn = Button(tools_frame, image=delete_image, bg='#a9c0e2', fg='black')
+delete_btn_hover = Hovertip(delete_btn, 'Delete', hover_delay=10)
+delete_btn.grid(row=0, column=4, padx=10, pady=5)
+
+close_image = PhotoImage(file='images/close.png', height=50, width=50)
+close_btn = Button(tools_frame, image=close_image, bg='#a9c0e2', fg='black')
+close_btn_hover = Hovertip(close_btn, 'Close', hover_delay=10)
+close_btn.grid(row=0, column=5, padx=10, pady=5)
 
 window.mainloop()
